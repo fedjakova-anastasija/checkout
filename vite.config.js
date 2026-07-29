5b8d86158a918e908051867bb8c5eea48343c997
anastasiya.fedyakova <anastasiya.fedyakova@travelline.ru>
2026-07-29 09:37:20 +0300

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
 
 module.exports = defineConfig({
+  base: '/checkout/',
   plugins: [react()],
   build: {
     outDir: 'build',
